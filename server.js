0869f1f3cc5c16d79bf0d9fb4f0ba2a93e6b7458
Sachini <sachichathudissanayaka@gmail.com>
2024-10-29 18:26:05 +0530

    Add changes

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const path = require("path");
+const port = process.env.PORT || 8000
 
 const app = express();
 
 // setup static folder
 // app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(8000, () => {
-  console.log("Server is running on port 8000");
+let posts = [
+  {id:1, title:"post 1"},
+  {id:2, title:"post 2"},
+  {id:3, title:"post 3"},
+]
+
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
